56977d2aea12cb23c536cdcc564593f14afd0c4b
Panu Lahtinen <pnuu+git@iki.fi>
2016-11-14 08:42:52 +0200

    Moved from trollduction.producer

---
+++ pytroll_collectors/file_notifiers.py
@@ -0,0 +1,80 @@
+import pyinotify
+
+# Generic event handler
+
+
+class EventHandler(pyinotify.ProcessEvent):
+
+    """Handle events with a generic *fun* function.
+    """
+
+    def __init__(self, fun, file_to_watch=None, item=None):
+        pyinotify.ProcessEvent.__init__(self)
+        self._file_to_watch = file_to_watch
+        self._item = item
+        self._fun = fun
+
+    def process_file(self, pathname):
+        '''Process event *pathname*
+        '''
+        if self._file_to_watch is None:
+            self._fun(pathname, self._item)
+        elif fnmatch(self._file_to_watch, os.path.basename(pathname)):
+            self._fun(pathname, self._item)
+
+    def process_IN_CLOSE_WRITE(self, event):
+        """On closing after writing.
+        """
+        self.process_file(event.pathname)
+
+    def process_IN_CREATE(self, event):
+        """On closing after linking.
+        """
+        try:
+            if os.stat(event.pathname).st_nlink > 1:
+                self.process_file(event.pathname)
+        except OSError:
+            return
+
+    def process_IN_MOVED_TO(self, event):
+        """On closing after moving.
+        """
+        self.process_file(event.pathname)
+
+
+class ConfigWatcher(object):
+
+    """Watch a given config file and run reload_config.
+    """
+
+    def __init__(self, config_file, config_item, reload_config):
+        mask = (pyinotify.IN_CLOSE_WRITE |
+                pyinotify.IN_MOVED_TO |
+                pyinotify.IN_CREATE)
+        self.config_file = config_file
+        self.config_item = config_item
+        self.watchman = pyinotify.WatchManager()
+
+        LOGGER.debug("Setting up watcher for %s", config_file)
+
+        self.notifier = \
+            pyinotify.ThreadedNotifier(self.watchman,
+                                       EventHandler(reload_config,
+                                                    os.path.basename(config_file
+                                                                     ),
+                                                    self.config_item
+                                                    )
+                                       )
+        self.watchman.add_watch(os.path.dirname(config_file), mask)
+
+    def start(self):
+        """Start the config watcher.
+        """
+        LOGGER.info("Start watching %s", self.config_file)
+        self.notifier.start()
+
+    def stop(self):
+        """Stop the config watcher.
+        """
+        LOGGER.info("Stop watching %s", self.config_file)
+        self.notifier.stop()
