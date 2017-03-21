e5d6ef513285cac86a025a37903ce0a8577cd962
Panu Lahtinen <pnuu+git@iki.fi>
2017-03-21 08:27:17 +0200

    Add function for sending messages

---
+++ pytroll_collectors/utils.py
@@ -0,0 +1,15 @@
+from posttroll.publisher import Publish
+from posttroll.message import Message
+
+
+def send_message(topic, msg_type, msg_data, nameservers=None, port=0):
+    """Send monitoring message"""
+    if nameservers is None:
+        nameservers = []
+    if not isinstance(nameservers, list):
+        nameservers = [nameservers]
+
+    with Publish("trollflow-sat monitor", port=port,
+                 nameservers=nameservers) as pub:
+        msg = Message(topic, msg_type, msg_data)
+        pub.send(str(msg))
