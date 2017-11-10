daed3c6cc03f4d82b01a4249ca0c89c1f65e96ce
Panu Lahtinen <pnuu+git@iki.fi>
2017-11-10 10:54:57 +0200

    Expose listener addresses, publish_port and rename nameservers to nameserver

--- bin/segment_gatherer.py
+++ bin/segment_gatherer.py
@@ -58,14 +58,26 @@ class SegmentGatherer(object):
         topics = config.get(section, 'topics').split()
 
         try:
-            nameservers = config.get(section, 'nameservers')
-            nameservers = nameservers.split(',')
+            nameserver = config.get(section, 'nameserver')
         except (NoOptionError, ValueError):
-            nameservers = []
+            nameserver = "localhost"
 
-        self._listener = ListenerContainer(topics=topics)
+        try:
+            addresses = config.get(section, 'addresses')
+            addresses = addresses.split()
+        except (NoOptionError, ValueError):
+            addresses = None
+
+        try:
+            publish_port = config.get(section, 'publish_port')
+        except NoOptionError:
+            publish_port = 0
+
+        self._listener = ListenerContainer(topics=topics, addresses=addresses,
+                                           nameserver=nameserver)
         self._publisher = publisher.NoisyPublisher("segment_gatherer",
-                                                   nameservers=nameservers)
+                                                   port=publish_port,
+                                                   nameserver=nameserver)
         self._subject = config.get(section, "publish_topic")
         self._pattern = config.get(section, 'pattern')
         self._parser = Parser(self._pattern)
