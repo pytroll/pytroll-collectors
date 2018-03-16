4776346614f8c2859f3d0d3cd316a15cebb9c970
Panu Lahtinen <pnuu+git@iki.fi>
2018-03-16 15:19:33 +0200

    Add unit test file for segment gatherer

---
+++ pytroll_collectors/tests/test_segments.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018 PyTroll team
+
+# Author(s):
+
+#   panu.lahtinen@fmi.fi
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit testing for segment gatherer
+"""
+
+import unittest
+
+from pytroll_collectors.segments import SegmentGatherer
+from helper_functions import read_yaml
+
+CONFIG_SINGLE = read_yaml("data/segments_single.yaml")
+# CONFIG_DOUBLE = read_yaml("data/segments_double.yaml")
+
+
+class TestSegmentGatherer(unittest.TestCase):
+
+    def setUp(self):
+        """Setting up the testing
+        """
+        g1 = SegmentGatherer(CONFIG_SIGLE)
+        # g2 = SegmentGatherer(CONFIG_DOUBLE)
+
+    def test_patterns(self):
+        pass
+
+    def tearDown(self):
+        """Closing down
+        """
+        pass
+
+
+def suite():
+    """The suite for test_trollduction
+    """
+    loader = unittest.TestLoader()
+    mysuite = unittest.TestSuite()
+    mysuite.addTest(loader.loadTestsFromTestCase(TestSegmentGatherer))
+
+    return mysuite
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite())
