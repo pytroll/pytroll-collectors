8654d7239095f0012190adcd357be74bbecc3a51
Martin Raspaud <martin.raspaud@smhi.se>
2019-09-18 19:21:18 +0200

    Fix missing satpy in tests

--- pytroll_collectors/tests/test_global_mosaic.py
+++ pytroll_collectors/tests/test_global_mosaic.py
@@ -29,7 +29,8 @@ from pyresample.geometry import AreaDefinition
 from pyresample.utils import proj4_str_to_dict
 from posttroll import message
 from posttroll.ns import NameServer
-
+import pytest
+satpy = pytest.importorskip("satpy")
 import pytroll_collectors.global_mosaic as gm
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
