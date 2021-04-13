2344d0344474663c555feff8eee3561f0c1ac432
Panu Lahtinen <pnuu+git@iki.fi>
2021-04-13 09:01:17 +0300

    Install netifaces and watchdog for tests

--- setup.py
+++ setup.py
@@ -62,5 +62,5 @@ setup(name="pytroll_collectors",
                         'trollsift', 'netifaces', 'watchdog',
                         'pytroll-schedule',
                         'six'],
-      tests_require=['trollsift', 'six'],
+      tests_require=['trollsift', 'six', 'netifaces', 'watchdog'],
       )
