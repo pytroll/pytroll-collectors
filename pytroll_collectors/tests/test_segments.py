8858da7c3d9fa4390d0dfa8b1d5688f751b017c8
Martin Raspaud <martin.raspaud@smhi.se>
2023-02-21 17:11:20 +0100

    Add test for fci and fix

--- pytroll_collectors/tests/test_segments.py
+++ pytroll_collectors/tests/test_segments.py
@@ -1216,7 +1216,7 @@ class TestFlooring(unittest.TestCase):
 
     def test_floor_time_10_minutes(self):
         """Test flooring 10 minutes."""
-        self.himawari_ini_message._adjust_time_by_flooring()
+        self.himawari_ini_message.adjust_time_by_flooring()
         self.assertEqual(20, self.himawari_ini_message.metadata['start_time'].minute)
 
     def test_floor_time_15_minutes(self):
@@ -1225,7 +1225,7 @@ class TestFlooring(unittest.TestCase):
             self.himawari_ini = SegmentGatherer(CONFIG_INI_HIMAWARI)
             self.himawari_ini_message = self.himawari_ini.message_from_posttroll(self.himawari_msg)
 
-            self.himawari_ini_message._adjust_time_by_flooring()
+            self.himawari_ini_message.adjust_time_by_flooring()
             self.assertEqual(15, self.himawari_ini_message.metadata['start_time'].minute)
 
     def test_floor_time_2_minutes(self):
@@ -1234,7 +1234,7 @@ class TestFlooring(unittest.TestCase):
             self.himawari_ini = SegmentGatherer(CONFIG_INI_HIMAWARI)
             self.himawari_ini_message = self.himawari_ini.message_from_posttroll(self.himawari_msg)
 
-            self.himawari_ini_message._adjust_time_by_flooring()
+            self.himawari_ini_message.adjust_time_by_flooring()
             self.assertEqual(28, self.himawari_ini_message.metadata['start_time'].minute)
 
     def test_floor_time_2_minutes_with_seconds(self):
@@ -1247,7 +1247,7 @@ class TestFlooring(unittest.TestCase):
             self.himawari_ini_message.metadata['start_time'] = dt.datetime(start_time.year, start_time.month,
                                                                            start_time.day, start_time.hour,
                                                                            start_time.minute, 42)
-            self.himawari_ini_message._adjust_time_by_flooring()
+            self.himawari_ini_message.adjust_time_by_flooring()
             self.assertEqual(self.himawari_ini_message.metadata['start_time'].minute, 28)
             self.assertEqual(self.himawari_ini_message.metadata['start_time'].second, 0)
 
@@ -1256,7 +1256,7 @@ class TestFlooring(unittest.TestCase):
         with patch.dict(CONFIG_INI_HIMAWARI, {'group_by_minutes': None}):
             self.himawari_ini = SegmentGatherer(CONFIG_INI_HIMAWARI)
             self.himawari_ini_message = self.himawari_ini.message_from_posttroll(self.himawari_msg)
-            self.himawari_ini_message._adjust_time_by_flooring()
+            self.himawari_ini_message.adjust_time_by_flooring()
             self.assertEqual(self.himawari_ini_message.metadata['start_time'].minute, 29)
 
 
@@ -1278,7 +1278,7 @@ class TestFlooringMultiplePatterns(unittest.TestCase):
 
     def test_floor_10_minutes(self):
         """Test flooring by 10 minutes."""
-        self.himawari_message._adjust_time_by_flooring()
+        self.himawari_message.adjust_time_by_flooring()
         self.assertEqual(self.himawari_message.metadata['start_time'].minute, 20)
 
     def test_floor_10_minutes_with_seconds_zeroed(self):
@@ -1287,7 +1287,7 @@ class TestFlooringMultiplePatterns(unittest.TestCase):
         self.himawari_message.metadata['start_time'] = dt.datetime(start_time.year, start_time.month,
                                                                    start_time.day, start_time.hour,
                                                                    start_time.minute, 42)
-        self.himawari_message._adjust_time_by_flooring()
+        self.himawari_message.adjust_time_by_flooring()
         self.assertEqual(self.himawari_message.metadata['start_time'].minute, 20)
         self.assertEqual(self.himawari_message.metadata['start_time'].second, 0)
 
@@ -1296,24 +1296,82 @@ class TestFlooringMultiplePatterns(unittest.TestCase):
         with patch.dict(CONFIG_DOUBLE_DIFFERENT['patterns']['himawari'], {'group_by_minutes': None}):
             self.iodc_himawari = SegmentGatherer(CONFIG_DOUBLE_DIFFERENT)
             self.himawari_message = self.iodc_himawari.message_from_posttroll(self.himawari_msg)
-            self.himawari_message._adjust_time_by_flooring()
+            self.himawari_message.adjust_time_by_flooring()
             self.assertEqual(29, self.himawari_message.metadata['start_time'].minute)
 
     def test_floor_grouping_does_not_affect_other_pattern(self):
         """Test that `group_by_minutes` for one pattern doesn't leak to the other patterns."""
         self.assertEqual(self.iodc_message.metadata['start_time'].minute, 15)
-        self.iodc_message._adjust_time_by_flooring()
+        self.iodc_message.adjust_time_by_flooring()
         self.assertEqual(self.iodc_message.metadata['start_time'].minute, 15)
 
 
-def suite():
-    """Test suite for test_trollduction."""
-    loader = unittest.TestLoader()
-    mysuite = unittest.TestSuite()
-    mysuite.addTest(loader.loadTestsFromTestCase(TestSegmentGatherer))
+fci_message_1 = ('pytroll://segment/fci_nc file mraspaud@4730366dc313 2023-02-21T13:45:47.413168 v1.01 application/json'
+                 ' {"path": "", "platform_name": "MTI1", "segment_type": "BODY", "processing_time": "20170920134515", '
+                 '"start_time": "2017-09-20T13:40:08", "end_time": "2017-09-20T13:40:15", "special_compression": "JLS",'
+                 ' "repeat_cycle_in_day": 83, "segment": "1", "uri": '
+                 '"/mnt/input/W_XX-EUMETSAT-Darmstadt,IMG+SAT,MTI1+FCI-1C-RRAD-FDHSI-FD--CHK-BODY---NC4E_C_EUMT_20170920134515_GTT_DEV_20170920134008_20170920134015_N_JLS_T_0083_0001.nc", '  # noqa
+                 '"uid": "W_XX-EUMETSAT-Darmstadt,IMG+SAT,MTI1+FCI-1C-RRAD-FDHSI-FD--CHK-BODY---NC4E_C_EUMT_20170920134515_GTT_DEV_20170920134008_20170920134015_N_JLS_T_0083_0001.nc", '  # noqa
+                 '"sensor": ["fci"]}')
 
-    return mysuite
+
+fci_message_2 = ('pytroll://segment/fci_nc file mraspaud@4730366dc313 2023-02-21T15:35:53.301660 v1.01 application/json'
+                 ' {"path": "", "platform_name": "MTI1", "segment_type": "BODY", "processing_time": "20170920153527", '
+                 '"start_time": "2017-09-20T15:30:09", "end_time": "2017-09-20T15:30:27", "special_compression": "JLS",'
+                 ' "repeat_cycle_in_day": 83, "segment": "2", "uri": '
+                 '"/mnt/input/W_XX-EUMETSAT-Darmstadt,IMG+SAT,MTI1+FCI-1C-RRAD-FDHSI-FD--CHK-BODY---NC4E_C_EUMT_20170920134527_GTT_DEV_20170920134009_20170920134027_N_JLS_T_0083_0002.nc",'  # noqa
+                 ' "uid": "W_XX-EUMETSAT-Darmstadt,IMG+SAT,MTI1+FCI-1C-RRAD-FDHSI-FD--CHK-BODY---NC4E_C_EUMT_20170920134527_GTT_DEV_20170920134009_20170920134027_N_JLS_T_0083_0002.nc",'  # noqa
+                 ' "sensor": ["fci"]}')
 
 
-if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(suite())
+FCI_CONFIG = """patterns:
+  fci_nc:
+    pattern:
+      "W_XX-EUMETSAT-Darmstadt,IMG+SAT,{platform_name:4s}+FCI-1C-RRAD-FDHSI-FD--CHK-{segment_type}---NC4E_C_EUMT_{processing_time}_GTT_DEV_{start_time:%Y%m%d%H%M%S}_{end_time:%Y%m%d%H%M%S}_N_{special_compression}_T_{repeat_cycle_in_day:>04d}_{segment:0>4s}.nc"
+    critical_files:
+    wanted_files: :0001-0040
+    all_files: :0001-0040
+    variable_tags: ["processing_time", "end_time", "segment_type"]
+    group_by_minutes: 10
+    time_tolerance: 600
+
+timeliness:
+  6000000
+
+time_name:
+  start_time
+
+posttroll:
+  topics:
+    - /segment/fci_nc
+  publish_topic:
+    /dataset/fci_nc
+
+"""
+
+
+class TestSegmentGathererFCI:
+    """Tests for the segment gatherer for FCI data."""
+
+    def test_process_message_fci_with_nonzero_seconds(self, caplog):
+        """Test processing a message."""
+        from posttroll.message import Message as Message_p
+        import yaml
+
+        segment_gatherer = SegmentGatherer(yaml.safe_load(FCI_CONFIG))
+
+        expected_uids = set()
+
+        for fci_message in [fci_message_1, fci_message_2]:
+            fci_msg = Message_p(rawstr=fci_message)
+            expected_uids.add(fci_msg.data['uid'])
+            msg = FakeMessage(fci_msg.data)
+            segment_gatherer.process(msg)
+
+        timestamp = '2017-09-20 13:40:00'
+        assert timestamp in segment_gatherer.slots
+        slot = segment_gatherer.slots[timestamp]
+        assert len(slot["fci_nc"]["received_files"]) == 2
+        assert len(slot.output_metadata["dataset"]) == 2
+        uids = set(info["uid"] for info in slot.output_metadata["dataset"])
+        assert uids == expected_uids
