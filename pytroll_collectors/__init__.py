6da6a0295cba3775521334431ce7866076f44ec1
Martin Raspaud <martin.raspaud@smhi.se>
2025-09-11 08:59:36 +0200

    Address review comments

--- pytroll_collectors/__init__.py
+++ pytroll_collectors/__init__.py
@@ -21,3 +21,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """The place where everything starts :)."""
+try:
+    from pytroll_collectors.version import version  # noqa
+except ModuleNotFoundError as err:  # pragma: no cover
+    err.add_note("This could mean you didn't install 'pytroll_collectors' properly.")
+    err.add_note("Try reinstalling ('pip install').")
+    raise
